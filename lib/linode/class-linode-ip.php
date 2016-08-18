7efccf0815c36c1b70c2f38f3e52833776a413ad
Doug Wollison <doug@wollison.net>
2016-08-17 21:19:32 -0400

    Added rdns/swap methods to Linode_IP

--- lib/linode/class-linode-ip.php
+++ lib/linode/class-linode-ip.php
@@ -45,4 +45,61 @@ class Linode_IP extends Asset {
 
 		return static::call_api( $action, $options );
 	}
+
+	/**
+	 * Set the rDNS name of a *public* IP.
+	 *
+	 * Updates the RDNS_NAME attribute when performed.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $hostname The hostname to set the reverse DNS to.
+	 */
+	public function set_rdns( $hostname ) {
+		$result = static::call_api( 'setrdns', array(
+			'IPAddressID' => $this->id,
+			'Hostname'    => $hostname,
+		) );
+
+		// Update the RDNS_NAME attribute
+		$this->attributes['RDNS_NAME'] = $result['HOSTNAME'];
+	}
+
+	/**
+	 * Transfer the IP Address to another linode.
+	 *
+	 * Updates the LINODEID attribute when performed.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $linode_id The ID of the linode to assign the IP to.
+	 */
+	public function transfer( $linode_id ) {
+		$result = static::call_api( 'swap', array(
+			'IPAddressID' => $this->id,
+			'toLinodeID' => $linode_id,
+		) );
+
+		// Update the LINODEID attribute
+		$this->attributes['LINODEID'] = $result[0]['LINODEID'];
+	}
+
+	/**
+	 * Swap the IP Address with another.
+	 *
+	 * Updates the LINODEID attribute when performed.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $ipaddress_id The ID of the IP Address to swap with.
+	 */
+	public function swap( $ipaddress_id ) {
+		$result = static::call_api( 'swap', array(
+			'IPAddressID' => $this->id,
+			'withIPAddressID' => $ipaddress_id,
+		) );
+
+		// Update the LINODEID attribute
+		$this->attributes['LINODEID'] = $result[0]['LINODEID'];
+	}
 }
