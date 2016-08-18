a78b319507a555d6211529072a66c5a360bff3d3
Doug Wollison <doug@wollison.net>
2016-08-17 22:15:19 -0400

    Initial logic for add_* methods

--- lib/linode/class-linode.php
+++ lib/linode/class-linode.php
@@ -58,4 +58,62 @@ class Linode extends API_Object {
 	 * @var array
 	 */
 	protected $ips = array();
+
+	/**
+	 * Create a new Config.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $options The config.create request parameters.
+	 *
+	 * @return Linode_Config The new Config object.
+	 */
+	public function add_config( array $options ) {
+		$options['LinodeID'] = $this->id;
+
+		$config = new Linode_Config( $options );
+
+		$this->configs[ $config->id ] = $config;
+
+		return $config;
+	}
+
+	/**
+	 * Create a new Disk.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $options The disk.create request parameters.
+	 *
+	 * @return Linode_Disk The new Disk object.
+	 */
+	public function add_disk( array $options ) {
+		$options['LinodeID'] = $this->id;
+
+		$disk = new Linode_Disk( $options );
+
+		$this->disks[ $disk->id ] = $disk;
+
+		return $disk;
+	}
+
+	/**
+	 * Create a new Config.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param bool $public Optional Wether the IP is public or not (default: TRUE).
+	 *
+	 * @return Linode_IP The new Config object.
+	 */
+	public function add_ip( $public = true ) {
+		$ip = new Linode_IP( array(
+			'LinodeID' => $this->id,
+			'IsPublic' => $public,
+		) );
+
+		$this->ips[ $ip->id ] = $ip;
+
+		return $ip;
+	}
 }
