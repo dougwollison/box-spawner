d9ffabcb898e27f4a90a9dbc32b8523b42f93178
Doug Wollison <doug@wollison.net>
2016-08-18 19:10:00 -0400

    Ditched add_config and add_disk since they're handled by the overloaded now.

--- lib/linode/class-linode.php
+++ lib/linode/class-linode.php
@@ -64,44 +64,6 @@ class Linode extends API_Object {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param array $options The config.create request parameters.
-	 *
-	 * @return Linode_Config The new Config object.
-	 */
-	public function add_config( array $options ) {
-		$options['LinodeID'] = $this->id;
-
-		$config = new Linode_Config( $options );
-
-		$this->configs[ $config->id ] = $config;
-
-		return $config;
-	}
-
-	/**
-	 * Create a new Disk.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param array $options The disk.create request parameters.
-	 *
-	 * @return Linode_Disk The new Disk object.
-	 */
-	public function add_disk( array $options ) {
-		$options['LinodeID'] = $this->id;
-
-		$disk = new Linode_Disk( $options );
-
-		$this->disks[ $disk->id ] = $disk;
-
-		return $disk;
-	}
-
-	/**
-	 * Create a new Config.
-	 *
-	 * @since 1.0.0
-	 *
 	 * @param bool $public Optional Wether the IP is public or not (default: TRUE).
 	 *
 	 * @return Linode_IP The new Config object.
