804f24c79a9e3f2e87c727e1f9d67a9455feb29f
Doug Wollison <doug@wollison.net>
2016-08-19 16:45:27 -0400

    Custom setup() handling; encode and add headers

--- lib/abstract-json-request.php
+++ lib/abstract-json-request.php
@@ -20,5 +20,19 @@ namespace BoxSpawner;
  * @since 1.0.0
  */
 abstract class JSON_Request {
-	// to be written
+	/**
+	 * Set the options on the cURL handle.
+	 *
+	 * Convert the data/body to JSON, add the type/length headers.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function setup() {
+		$this->data = json_encode( $this->data );
+
+		$this->headers[] = 'Content-Type: application/json';
+		$this->headers[] = 'Content-Length: ' . strlen( $this->data );
+
+		parent::setup();
+	}
 }
