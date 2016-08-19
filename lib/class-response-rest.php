ebada250fb1a53cae88483d8062709b52b99d1a2
Doug Wollison <doug@wollison.net>
2016-08-19 14:56:43 -0400

    Fleshed out REST response handling

--- lib/class-response-rest.php
+++ lib/class-response-rest.php
@@ -20,5 +20,31 @@ namespace BoxSpawner;
  * @since 1.0.0
  */
 class Response_REST extends Response_JSON {
-	// to be written
+	/**
+	 * Get the results of the request.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array The JSON decoded result data.
+	 */
+	public function result() {
+		// Get the decoded result
+		$result = parent::result();
+
+		$json = $result['body'];
+
+		if ( ! isset( $json['result'] ) ) {
+			throw new Exception( 'Unrecognized response format. "result" entry should be present.' );
+		}
+
+		if ( isset( $json['errors'] ) && count( $json['errors'] ) > 0 ) {
+			$error = $json['errors'][0];
+			throw new Exception( 'REST API Error: ' . $error['message'] );
+		}
+
+		// Get the response data
+		$data = $json['result'];
+
+		return $data;
+	}
 }
