022e69bc86cccb71101bd893a44b5589344acac4
Doug Wollison <doug@wollison.net>
2016-08-19 16:42:34 -0400

    New JSON_Request framework

---
+++ lib/abstract-json-request.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The JSON Request Object.
+ *
+ * @package Box_Spawner
+ * @subpackage Framework
+ *
+ * @since 1.0.0
+ */
+
+namespace BoxSpawner;
+
+/**
+ * The JSON Request class.
+ *
+ * An interface for creating cURL requests to APIs that require a JSON body.
+ *
+ * @api
+ *
+ * @since 1.0.0
+ */
+abstract class JSON_Request {
+	// to be written
+}
