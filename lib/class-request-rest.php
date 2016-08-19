0e335ff33da29ea71cf9d7a9c8515dc1aa0ef1a9
Doug Wollison <doug@wollison.net>
2016-08-19 14:49:30 -0400

    Started REST request/response framework

---
+++ lib/class-request-rest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The REST Request Object.
+ *
+ * @package Box_Spawner
+ * @subpackage Framework
+ *
+ * @since 1.0.0
+ */
+
+namespace BoxSpawner;
+
+/**
+ * The REST Request class.
+ *
+ * An interface for creating cURL requests for RESTful APIs.
+ *
+ * @api
+ *
+ * @since 1.0.0
+ */
+class Request {
+	// to be written
+}
---
+++ lib/class-response-rest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The Response Object.
+ *
+ * @package Box_Spawner
+ * @subpackage Framework
+ *
+ * @since 1.0.0
+ */
+
+namespace BoxSpawner;
+
+/**
+ * The Response class.
+ *
+ * An interface for handling the result of cURL requests of RESTful APIs.
+ *
+ * @api
+ *
+ * @since 1.0.0
+ */
+class Response_REST extends Response_JSON {
+	// to be written
+}
