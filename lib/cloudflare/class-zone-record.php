bcdcb51fddedb7ba27ed8dd3614d6c7addc25ced
Doug Wollison <doug@wollison.net>
2016-08-19 15:19:23 -0400

    Basic Zone/Record classes for now

---
+++ lib/cloudflare/class-zone-record.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The Zone Record Object.
+ *
+ * @package Box_Spawner
+ * @subpackage CloudFlare
+ *
+ * @since 1.0.0
+ */
+namespace BoxSpawner\CloudFlare;
+
+/**
+ * The Zone Record class.
+ *
+ * An interface for creating/manipulating CloudFlare zone records.
+ *
+ * @internal Used by the Droplet class.
+ *
+ * @since 1.0.0
+ */
+class Zone_Record extends API_Object {
+	// to be written
+}
---
+++ lib/cloudflare/class-zone.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The Zone Object.
+ *
+ * @package Box_Spawner
+ * @subpackage CloudFlare
+ *
+ * @since 1.0.0
+ */
+namespace BoxSpawner\CloudFlare;
+
+/**
+ * The Zone class.
+ *
+ * An interface for creating/manipulating CloudFlare zones.
+ *
+ * @api
+ *
+ * @since 1.0.0
+ */
+class Zone extends API_Object {
+	// to be written
+}
