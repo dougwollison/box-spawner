5ec95b9b9b9ea8b19d04437b04fdeb7dc41931e3
Doug Wollison <doug@wollison.net>
2016-08-19 17:39:43 -0400

    Need slightly modified versions of the Dependable trait for each use

---
+++ lib/linode/trait-dependable.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The Dependable Framework.
+ *
+ * @package Box_Spawner
+ * @subpackage Linode
+ *
+ * @since 1.0.0
+ */
+namespace BoxSpawner\Linode;
+
+/**
+ * The Dependable trait.
+ *
+ * @internal Used by Linode objects that support child objects.
+ *
+ * @since 1.0.0
+ */
+trait Dependable {
+	use \BoxSpawner\Dependable;
+
+	/**
+	 * Load all child objects of a type for this object.
+	 *
+	 * All returned objects will be added to the matching list.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $type The type of object to load.
+	 */
+	public function load_children( $type ) {
+		// Get the class name for the child object
+		$class = get_class( $this ) . '_' . ucwords( $type );
+
+		$entries = $class::all( array(
+			static::ID_ATTRIBUTE => $this->id,
+		) );
+
+		foreach ( $entries as $entry ) {
+			$id = $entry['id'];
+
+			$object = new $class( $id, $entry );
+
+			$this->{"{$type}s"}[ $object->id ] = $object;
+		}
+	}
+}
