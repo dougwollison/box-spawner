dbf633bad830d9b46a1b0915d9a8e2b3611bf991
Doug Wollison <doug@wollison.net>
2016-08-19 15:06:16 -0400

    Basics of REST request class

--- lib/class-request-rest.php
+++ lib/class-request-rest.php
@@ -20,5 +20,35 @@ namespace BoxSpawner;
  * @since 1.0.0
  */
 class Request {
-	// to be written
+	/**
+	 * The class constructor.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $action The specific action/endpoint for the request.
+	 * @param array  $data   Optional The data to send in the request.
+	 * @param string $method Optional The HTTP method to use for the request (defaults to "GET").
+	 */
+	public function __construct( $endpoint, array $data = array(), $method = 'GET' ) {
+		$this->endpoint = $endpoint;
+		$this->data     = $data;
+		$this->method   = strtoupper( $method ); // ensure method is uppercase
+		$this->handle   = curl_init();
+
+		$this->setup();
+
+		$this->result = curl_exec( $this->handle );
+	}
+
+	/**
+	 * Set the options on the cURL handle.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function setup() {
+		// Set the request method
+		$this->options[ CURLOPT_CUSTOMREQUEST ] = $this->method;
+
+		parent::setup();
+	}
 }
