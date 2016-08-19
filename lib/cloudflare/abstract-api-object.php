e045d3661ec8e39f233398b5d784931096f3e318
Doug Wollison <doug@wollison.net>
2016-08-19 15:06:27 -0400

    Add base for api_object

---
+++ lib/cloudflare/abstract-api-object.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The CloudFlare Object Framework.
+ *
+ * @package Box_Spawner
+ * @subpackage CloudFlare
+ *
+ * @since 1.0.0
+ */
+namespace BoxSpawner\CloudFlare;
+
+/**
+ * The API Object class.
+ *
+ * The base interface for all CloudFlare objects.
+ *
+ * @internal Extended by other CloudFlare object classes.
+ *
+ * @since 1.0.0
+ */
+abstract class API_Object extends \BoxSpawner\API_Object {
+	// to be written
+}
