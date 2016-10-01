e8d8a9d60d1b79c28c06f65fa96f996316686aa4
Doug Wollison <doug@wollison.net>
2016-10-01 19:27:43 -0400

    Pass parent id instead of whole object
    also added PARENT_ID_ATTRIBUTE for auto-guessing parent id based on data

--- framework/abstract-api-asset.php
+++ framework/abstract-api-asset.php
@@ -20,27 +20,40 @@ namespace BoxSpawner;
  */
 abstract class API_Asset extends API_Object {
 	/**
-	 * The ID of the object.
+	 * The name of the option to assign the parent ID to.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	const PARENT_ID_ATTRIBUTE = 'id';
+
+	/**
+	 * The ID of the parent object.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @var int
 	 */
-	public $parent;
+	public $parent_id;
 
 	/**
 	 * Create a new object or retrieve and existing one.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param BoxSpawner\API        $api    The API to use for this object.
-	 * @param int|string            $id     Either an ID of an existing one, or NULL to create one.
-	 * @param int|array             $data   Either the attributes for an already fetched one, or the options for creating one.
-	 * @param BoxSpawner\API_Object $parent The parent object to tie to.
+	 * @param BoxSpawner\API $api    The API to use for this object.
+	 * @param int|string     $id     Either an ID of an existing one, or NULL to create one.
+	 * @param int|array      $data   Either the attributes for an already fetched one, or the options for creating one.
+	 * @param int|string     $parent_id The parent object to tie to.
 	 */
-	public function __construct( API $api, $id, array $data = array(), $parent = null ) {
+	public function __construct( API $api, $id, array $data = array(), $parent_id = null ) {
 		// Store the parent
-		$this->parent = $parent;
+		if ( ! is_null( $parent_id ) ) {
+			$this->parent_id = $parent_id;
+		} else if ( isset( $data[ $this::PARENT_ID_ATTRIBUTE ] ) ) {
+			$this->parent_id = $data[ $this::PARENT_ID_ATTRIBUTE ];
+		}
 
 		parent::__construct( $api, $id, $data );
 	}
