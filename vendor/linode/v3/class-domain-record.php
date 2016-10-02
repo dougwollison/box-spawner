1d216510d284180ff0a9cb31d83c9cdc3932e936
Doug Wollison <doug@wollison.net>
2016-10-02 01:07:03 -0400

    Finished domain/record/stackscript classes

---
+++ vendor/linode/v3/class-domain-record.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * The Linode Record Object (version 3).
+ *
+ * @package Box_Spawner
+ * @subpackage Linode
+ *
+ * @since 1.0.0
+ */
+namespace BoxSpawner\Linode\V3;
+
+/**
+ * The Linode Record object class.
+ *
+ * The an absraction of the Linode Record api object.
+ *
+ * @api
+ *
+ * @since 1.0.0
+ */
+class Domain_Record extends API_Asset implements \BoxSpawner\Linode\Domain_Record_Framework {
+	/**
+	 * The name of the option to assign the ID to.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	const PARENT_ID_ATTRIBUTE = Domain::ID_ATTRIBUTE;
+
+	/**
+	 * The name of the option to assign the ID to.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	const ID_ATTRIBUTE = 'RESOURCEID';
+
+	/**
+	 * Issue a create request for a new object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data The data for the create request.
+	 */
+	protected function create( array $data ) {
+		$result = $this->api->create_domain_record( $this->parent_id, $data, 'array' );
+
+		$this->load( $result[ $this::ID_ATTRIBUTE ] );
+	}
+
+	/**
+	 * Issue a load request for a new object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $id The id of the object to load.
+	 */
+	protected function load( $id ) {
+		$this->id = $id;
+		$this->attributes = $this->api->get_domain_record( $this->parent_id, $id, 'array' );
+	}
+
+	/**
+	 * Issue a update request for a new object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data The data for the update request.
+	 */
+	public function update( array $data ) {
+		return $this->api->update_domain_record( $this->parent_id, $this->id, $data );
+	}
+
+	/**
+	 * Issue a delete request for a new object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Optional UNUSED.
+	 */
+	public function delete( array $data = array() ) {
+		return $this->api->delete_domain_record( $this->parent_id, $this->id );
+	}
+}
---
+++ vendor/linode/v3/class-stackscript.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The Stackscript Object (version 3).
+ *
+ * @package Box_Spawner
+ * @subpackage Linode
+ *
+ * @since 1.0.0
+ */
+namespace BoxSpawner\Linode\V3;
+
+/**
+ * The Stackscript object class.
+ *
+ * The an absraction of the Stackscript api object.
+ *
+ * @api
+ *
+ * @since 1.0.0
+ */
+class Stackscript extends API_Object implements \BoxSpawner\Linode\Linode_Framework {
+	/**
+	 * The name of the option to assign the ID to.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	const ID_ATTRIBUTE = 'DOMAINID';
+
+	/**
+	 * Issue a create request for a new object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data The data for the create request.
+	 */
+	protected function create( array $data ) {
+		$result = $this->api->create_stackscript( $data, 'array' );
+
+		$this->load( $result[ $this::ID_ATTRIBUTE ] );
+	}
+
+	/**
+	 * Issue a load request for a new object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $id The id of the object to load.
+	 */
+	protected function load( $id ) {
+		$this->id = $id;
+		$this->attributes = $this->api->get_stackscript( $id, 'array' );
+	}
+
+	/**
+	 * Issue a update request for a new object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data The data for the update request.
+	 */
+	public function update( array $data ) {
+		return $this->api->update_stackscript( $this->id, $data );
+	}
+
+	/**
+	 * Issue a delete request for a new object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Optional The data for the delete request.
+	 */
+	public function delete( array $data = array() ) {
+		return $this->api->delete_stackscript( $this->id, $data );
+	}
+}
