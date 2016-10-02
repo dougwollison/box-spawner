d3411c8580657845b7f50ba001d08bcfada8dad1
Doug Wollison <doug@wollison.net>
2016-10-02 00:43:45 -0400

    Subsequent exceptions should extend main one

--- exceptions.php
+++ exceptions.php
@@ -23,39 +23,39 @@ class Exception extends \Exception {}
  *
  * @since 1.0.0
  */
-class MissingParameterException extends \Exception {}
+class MissingParameterException extends Exception {}
 
 /**
  * For invalid paramters...
  *
  * @since 1.0.0
  */
-class InvalidParameterException extends \Exception {}
+class InvalidParameterException extends Exception {}
 
 /**
  * For actions that are not supported...
  *
  * @since 1.0.0
  */
-class NotSupportedException extends \Exception {}
+class NotSupportedException extends Exception {}
 
 /**
  * For errors with PHP resources...
  *
  * @since 1.0.0
  */
-class ResourceException extends \Exception {}
+class ResourceException extends Exception {}
 
 /**
  * For invalid responses...
  *
  * @since 1.0.0
  */
-class InvalidResponseException extends \Exception {}
+class InvalidResponseException extends Exception {}
 
 /**
  * For API response errors...
  *
  * @since 1.0.0
  */
-class ErrorResponseException extends \Exception {}
+class ErrorResponseException extends Exception {}
