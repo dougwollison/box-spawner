0fd73d28513c94d5bb5d487e8efeca4befe0bf5f
Doug Wollison <doug@wollison.net>
2016-10-03 20:50:39 -0400

    Add record methods to domain

--- vendor/linode/v3/class-domain.php
+++ vendor/linode/v3/class-domain.php
@@ -74,4 +74,78 @@ class Domain extends \BoxSpawner\API_Object implements \BoxSpawner\Linode\Domain
 	public function delete( array $data = array() ) {
 		return $this->api->delete_domain( $this->id );
 	}
+
+	// ==================================================
+	// ! Asset Actions
+	// ==================================================
+
+	// =========================
+	// ! - Record Assets
+	// =========================
+
+	/**
+	 * Retrieve a list of records.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $filter Optional Arguments for filtering the list request.
+	 *
+	 * @return array The list of Domain_Record objects.
+	 */
+	public function list_records( array $filter = array() ) {
+		return $this->api->list_domain_records( $this->id, $filter );
+	}
+
+	/**
+	 * Retrieve a single record.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $record_id The ID of the record to retrieve.
+	 *
+	 * @return Domain_Record The record object.
+	 */
+	public function get_record( $record_id ) {
+		return $this->api->get_domain_record( $this->id, $record_id );
+	}
+
+	/**
+	 * Create a new record.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data The properties of the new record.
+	 *
+	 * @return Domain_Record The record object.
+	 */
+	public function create_record( $data ) {
+		return $this->api->create_domain_record( $this->id, $data );
+	}
+
+	/**
+	 * Update an existing record.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int   $record_id The ID of the record to update.
+	 * @param array $data      The properties of the new Linode.
+	 *
+	 * @return bool Wether or not the update was successful.
+	 */
+	public function update_record( $record_id, array $data ) {
+		return $this->api->update_domain_record( $this->id, $record_id, $data );
+	}
+
+	/**
+	 * Delete an existing record.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $record_id The ID of the record to delete.
+	 *
+	 * @return bool Wether or not the delete was successful.
+	 */
+	public function delete_record( $record_id ) {
+		return $this->api->delete_domain_record( $this->id, $record_id );
+	}
 }
