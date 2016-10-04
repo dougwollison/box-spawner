278d420edf4f8d91e34d2b054a28cba9446e763f
Doug Wollison <doug@wollison.net>
2016-10-03 23:52:32 -0400

    Define asset actions in Domain framework interface

--- vendor/linode/interface-domain-framework.php
+++ vendor/linode/interface-domain-framework.php
@@ -16,5 +16,67 @@ namespace BoxSpawner\Linode;
  * @since 1.0.0
  */
 interface Domain_Framework {
-	// nothing special
+	// ==================================================
+	// ! Asset Actions
+	// ==================================================
+
+	// =========================
+	// ! - Record Assets
+	// =========================
+
+	/**
+	 * Retrieve a list of records.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $filter Optional Arguments for filtering the list request.
+	 *
+	 * @return array The list of Domain_Record objects.
+	 */
+	public function list_records( array $filter = array() );
+
+	/**
+	 * Retrieve a single record.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $record_id The ID of the record to retrieve.
+	 *
+	 * @return Domain_Record The record object.
+	 */
+	public function get_record( $record_id );
+
+	/**
+	 * Create a new record.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data The properties of the new record.
+	 *
+	 * @return Domain_Record The record object.
+	 */
+	public function create_record( $data );
+
+	/**
+	 * Update an existing record.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int   $record_id The ID of the record to update.
+	 * @param array $data      The properties of the new Linode.
+	 *
+	 * @return bool Wether or not the update was successful.
+	 */
+	public function update_record( $record_id, array $data );
+
+	/**
+	 * Delete an existing record.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $record_id The ID of the record to delete.
+	 *
+	 * @return bool Wether or not the delete was successful.
+	 */
+	public function delete_record( $record_id );
 }
